670da4dff6b67f963f2e5285ff74e6f2a807fa55
DESKTOP-S4EB9D6\Alphalives <p.phetsinorath@alphalives.com>
2019-04-15 14:04:22 +0200

    modification des offres de l'index

--- Bull's eye/vue/vue_anonces_accueil.php
+++ Bull's eye/vue/vue_anonces_accueil.php
@@ -1,22 +1,19 @@
 <?php
     if(isset($resultats) && $resultats!=null){
 
-        echo "<table border = 1>
-        <tr> <td> Photo </td><td> Annonce </td> <td> Jeux </td><td> Qui? </td><td> Age </td><td> Categorie </td><td> Description </td></tr> ";
-
         foreach ($resultats as $unRes)
         {
             echo '<div class="col-sm-4">
             <div class="card">
               <div class="img-jeu">
-              <img src="img/trone.jpg" class="thumbnail" alt="article">
+              <img src="'.$unRes['url'].'" class="thumbnail" alt="article">
               </div>
               <hr>
               <div class="card-body">
-                <h4 class="card-title">Nom du jeu</h4>
+                <h4 class="card-title">'.$unRes['designation'].'</h4>
                 <p class="card-text"><i class="fas fa-gamepad"></i> Type de jeu</p>
                 <p class="card-text"><i class="fas fa-map-marker-alt"></i> Lieu</p>
-                <p class="card-text"><i class="fas fa-users"></i> Age</p>
+                <p class="card-text"><i class="fas fa-users"></i> '.$unRes['age_requis'].'</p>
                 <a href="#" class="btn btn-outline-secondary">Voir plus</a>
               </div>
             </div>
