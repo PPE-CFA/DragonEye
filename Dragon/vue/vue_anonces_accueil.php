ae1cdd925a4d83aaf5842a67343c6f56f86689f7
pierre <phpierre@hotmail.fr>
2019-04-22 23:35:36 +0200

    fix

---
+++ Dragon/vue/vue_anonces_accueil.php
@@ -0,0 +1,25 @@
+<?php
+    if(isset($resultats) && $resultats!=null){
+
+        foreach ($resultats as $unRes)
+        {
+            echo '<div class="col-sm-4">
+            <div class="card">
+              <div class="img-jeu">
+              <img src="'.$unRes['url'].'" class="thumbnail" alt="article">
+              </div>
+              <hr>
+              <div class="card-body">
+                <h4 class="card-title">'.$unRes['designation'].'</h4>
+                <p class="card-text"><i class="fas fa-gamepad"></i> Type de jeu</p>
+                <p class="card-text"><i class="fas fa-map-marker-alt"></i> Lieu</p>
+                <p class="card-text"><i class="fas fa-users"></i> '.$unRes['age_requis'].'</p>
+                <a href="#" class="btn btn-outline-secondary">Voir plus</a>
+              </div>
+            </div>
+          </div>';
+        }
+
+        echo "</table>";
+    }
+?>
