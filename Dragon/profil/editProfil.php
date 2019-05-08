3c06f7113c3531879e2a176715febe682d59b248
PHETpierre <phpierre@hotmail.fr>
2019-05-08 23:28:28 +0200

    update

--- Dragon/profil/editProfil.php
+++ Dragon/profil/editProfil.php
@@ -1,57 +1,60 @@
 
 <?php
-session_start();
-
-//requete SQL : modifie le nom, le prenom, le mail, et le mdp de l'utilisateur de la table deye_personne
-
-$bdd = new PDO('mysql:host=127.0.0.1;dbname=bdd_jeu;charset=utf8', 'root', '');
+include('../include/header.php');
 
 if(isset($_SESSION['IdPersonne']))
 {
-   $requser = $bdd->prepare("SELECT * FROM deye_personne WHERE IdPersonne = ?");
-   $requser->execute(array($_SESSION['IdPersonne']));
-   $user = $requser->fetch();
-
    if(isset($_POST['newNom']) AND !empty($_POST['newNom']) AND $_POST['newNom'] != $user['nom']){
       
       $newNom = htmlspecialchars($_POST['newNom']);
-      $u = "U";
-      $updateNom = $bdd->prepare("UPDATE deye_personne SET nom = ? WHERE IdPersonne = ? AND IdType = ?");
-      $updateNom->execute(array($newNom,$_SESSION['IdPersonne'],$u));
+      //$u = "U";
+      //$updateNom = $bdd->prepare("UPDATE deye_personne SET nom = ? WHERE IdPersonne = ? AND IdType = ?");
+      $updateNom = $bdd->prepare("UPDATE deye_personne SET nom = ? WHERE IdPersonne = ? ");
+      //$updateNom->execute(array($newNom,$_SESSION['IdPersonne'],$u));
+      $updateNom->execute(array($newNom,$_SESSION['IdPersonne']));
       
    }
 
    if(isset($_POST['newPrenom']) AND !empty($_POST['newPrenom']) AND $_POST['newPrenom'] != $user['prenom']){
       
       $newPrenom = htmlspecialchars($_POST['newPrenom']);
-      $u = "U";
-      $updatePrenom = $bdd->prepare("UPDATE deye_personne SET prenom = ? WHERE IdPersonne = ? AND IdType = ?");
-      $updatePrenom->execute(array($newPrenom,$_SESSION['IdPersonne'],$u));
+      //$u = "U";
+      //$updatePrenom = $bdd->prepare("UPDATE deye_personne SET prenom = ? WHERE IdPersonne = ? AND IdType = ?");
+      $updatePrenom = $bdd->prepare("UPDATE deye_personne SET prenom = ? WHERE IdPersonne = ? ");
+      //$updatePrenom->execute(array($newPrenom,$_SESSION['IdPersonne'],$u));
+      $updatePrenom->execute(array($newPrenom,$_SESSION['IdPersonne']));
       
    }
 
    if(isset($_POST['newMail']) AND !empty($_POST['newMail']) AND $_POST['newMail'] != $user['email']){
       
       $newMail = htmlspecialchars($_POST['newMail']);
-      $u = "U";
-      $updateMail = $bdd->prepare("UPDATE deye_personne SET email = ? WHERE IdPersonne = ? AND IdType = ?");
-      $updateMail->execute(array($newMail,$_SESSION['IdPersonne'],$u));
-      
+      //$u = "U";
+      //$updateMail = $bdd->prepare("UPDATE deye_personne SET email = ? WHERE IdPersonne = ? AND IdType = ?");
+      $updateMail = $bdd->prepare("UPDATE deye_personne SET email = ? WHERE IdPersonne = ? ");
+      //$updateMail->execute(array($newMail,$_SESSION['IdPersonne'],$u));
+      $updateMail->execute(array($newMail,$_SESSION['IdPersonne']));
    }
 
    if(isset($_POST['newMdp']) AND !empty($_POST['newMdp']) AND isset($_POST['newMdp2']) AND !empty($_POST['newMdp2'])) {
       
       $newMdp = ($_POST['newMdp']);
       $newMdp2 = ($_POST['newMdp2']);
-      $u = "U";
+      //$u = "U";
       if($newMdp == $newMdp2){
-         $updateMdp = $bdd->prepare("UPDATE deye_personne SET mdp = ? WHERE IdPersonne = ? AND IdType = ?");
-         $updateMdp->execute(array($newMdp,$_SESSION['IdPersonne'],$u));
+         //$updateMdp = $bdd->prepare("UPDATE deye_personne SET mdp = ? WHERE IdPersonne = ? AND IdType = ?");
+         $updateMdp = $bdd->prepare("UPDATE deye_personne SET mdp = ? WHERE IdPersonne = ? ");
+         //$updateMdp->execute(array($newMdp,$_SESSION['IdPersonne'],$u));
+         $updateMdp->execute(array($newMdp,$_SESSION['IdPersonne']));
       }else{
          $erreur = "Vos deux mots de passe ne correspondent pas !";
       }
       
    }
+
+   $requser = $bdd->prepare("SELECT * FROM deye_personne WHERE IdPersonne = ?");
+   $requser->execute(array($_SESSION['IdPersonne']));
+   $user = $requser->fetch();
 
 ?>
 <html>
@@ -71,14 +74,13 @@ if(isset($_SESSION['IdPersonne']))
 
 
 <body>
-  <?php include('../include/header.php') ?>
   <!-- Formulaire de modification d'un utilisateur-->
    <div class="container-fluid padding">
    <div class="row text-center padding">
 
-         <div class="container center_div">
+         <div class="center_div col-6 m-auto">
           <div class="row">
-         <form method="POST" class="form-signup">
+         <form method="POST" class="form-signup m-auto col-10">
             <div class="sign_up">
             <h4 class="card-title text-center">Modifier mon profil</h4>
               <div class="form-label-group">
@@ -101,16 +103,14 @@ if(isset($_SESSION['IdPersonne']))
                 <label for="inputMdp2">Confirmer mot de passe</label>
                 <input type="password" id="newMdp2" class="form-control" name="newMdp2" placeholder="Confirmer mot de passe">
               </div>
-
+              <?php 
+                  if (isset($erreur)){
+                     echo '<div class="alert alert-danger">'.$erreur.'</div>';
+                  }
+               ?>
               <button class="btn btn-lg btn-secondary btn-block text-uppercase" type="submit">Je modifie</button>
               </div>
          </form>
-         <?php 
-            if (isset($erreur)){
-               echo $erreur;
-            }
-    
-         ?>
       </div>
       </div>
 
