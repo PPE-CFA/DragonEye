b49a448a93039853d2eccc9760e44e5f75db13bd
LakunCindy <43467524+LakunCindy@users.noreply.github.com>
2019-06-05 08:38:51 +0200

    Update sec.php plateforme

--- Dragon/include/sec.php
+++ Dragon/include/sec.php
@@ -1,12 +1,14 @@
 <?php
-    define("_DEV_", false);
+    define("_DEV_", true);
     if(_DEV_){
         //define("_DIR_", "http://localhost/dragon/Dragon");
         //define("_DIR2_", $_SERVER['DOCUMENT_ROOT'].'/dragon/Dragon');
-        define("_DIR_", "http://localhost/dragoneye/Dragon");
-        define("_DIR2_", $_SERVER['DOCUMENT_ROOT'].'/dragoneye/Dragon');
+        //define("_DIR_", "http://localhost/dragoneye/Dragon");
+        //define("_DIR2_", $_SERVER['DOCUMENT_ROOT'].'/dragoneye/Dragon');
         //define("_DIR_", "http://localhost/Drag/Dragon");
         //define("_DIR2_", $_SERVER['DOCUMENT_ROOT'].'/Drag/Dragon');
+        define("_DIR_", "http://10.20.20.3/DragonEye/Dragon");
+        define("_DIR2_", $_SERVER['DOCUMENT_ROOT'].'/DragonEye/Dragon');
     }else{
         define("_DIR_", "http://dragon-eye.mediadev.info");
         define("_DIR2_", $_SERVER['DOCUMENT_ROOT']);
